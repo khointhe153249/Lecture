26075e91f5f3ebd254714d6f5dd50fe66142f6c6
khointhe153249 <khointhe153249@fpt.edu.vn>
2021-06-21 18:02:40 +0700

    Change position

--- SEM 2/SE1613/src/BTVN/BTString.java
+++ SEM 2/SE1613/src/BTVN/BTString.java
@@ -1,4 +1,4 @@
-package BTString;
+package BTVN;
 
 import java.util.Scanner;
 
--- SEM 2/SE1613/src/BTVN/CheckNumber.java
+++ SEM 2/SE1613/src/BTVN/CheckNumber.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package CheckNumber;
+package BTVN;
 
 import java.util.Scanner;
 
@@ -11,7 +11,7 @@ import java.util.Scanner;
  *
  * @author acer
  */
-public class Main {
+public class CheckNumber {
 
     public static Scanner sc = new Scanner(System.in);
 
