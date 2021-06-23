04ee9cd39ed25e918397d6d01ec4ef648c49b08d
khointhe153249 <khointhe153249@fpt.edu.vn>
2021-06-23 20:31:38 +0700

    add resource

---
+++ SEM 2/SE1613/src/Sanfoundry/Shelf.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Sanfoundry;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author acer
+ */
+public class Shelf {
+
+    private String coLor;
+    private String name;
+
+    private Scanner sc = new Scanner(System.in);
+    private int amount=500;
+    private Student[] list = new Student[amount];
+
+    public Shelf() {
+    }
+
+    public Shelf(String coLor, String name, Student list[]) {
+        this.coLor = coLor;
+        this.name = name;
+        this.list = new Student[amount];
+    }
+
+    public String getCoLor() {
+        return coLor;
+    }
+
+    public void setCoLor(String coLor) {
+        this.coLor = coLor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Student[] getList() {
+        return list;
+    }
+
+    public void setList(Student[] list) {
+        this.list = list;
+    }
+
+    public void input() {
+        System.out.print("Input color shelf: ");
+        coLor = sc.nextLine();
+        System.out.print("Input name shelf: ");
+        name = sc.nextLine();
+
+        System.out.println("Input list file: ");
+        System.out.print("Input amount file: ");
+        amount = Integer.parseInt(sc.nextLine());
+
+        for (int i = 0; i < amount; i++) {
+            System.out.println("----- File " + (i + 1) + " ------");
+            System.out.print("Input id: ");
+            String id = sc.nextLine();
+            System.out.print("Input name: ");
+            String namef = sc.nextLine();
+            System.out.print("Input age: ");
+            int age = Integer.parseInt(sc.nextLine());
+            System.out.print("Input gpa: ");
+            double gpa = Double.parseDouble(sc.nextLine());
+            list[i] = new Student(id, namef, age, gpa);
+        } 
+    }
+    
+    public void output() {
+        System.out.println("Color shelf: " + coLor);
+        System.out.println("Name shelf: " + name);
+        for (int i = 0; i < amount; i++) {
+            System.out.println(list[i]);
+        }
+    }
+}
---
+++ SEM 2/SE1613/src/Sanfoundry/UsingShelf.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Sanfoundry;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author acer
+ */
+public class UsingShelf {
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void memu() {
+        System.out.println("---------- FPT Academic Portal ----------");
+        System.out.println("1.Add shelf.");
+        System.out.println("2.Print shelf.");
+        System.out.println("3.Search shelf by name.");
+        System.out.println("4.Update shelf by name.");
+        System.out.println("5.Remove shelf.");
+        System.out.println("6.Sort shelf by id");
+        System.out.println("7.Remove shelf.");
+        System.out.println("8.Exit FAP");
+        System.out.println("---------- Finish ---------");
+        System.out.print("Input choice: ");
+    }
+
+    public static int amount = 0;
+
+    public static void app(Shelf[] s) {
+        while (true) {
+            memu();
+            int choice = Integer.parseInt(sc.nextLine());
+
+//            int amount = 500;
+            switch (choice) {
+                case 1: {
+                    System.out.print("Input amount shelf: ");
+                    amount = Integer.parseInt(sc.nextLine());
+                    for (int i = 0; i < amount; i++) {
+                        System.out.println("---------- Shelf " + i + " ----------");
+                        s[i] = new Shelf();
+                        s[i].input();
+                    }
+                    break;
+                }
+
+                case 2: {
+                    for (int i = 0; i < amount; i++) {
+                        System.out.println("---------- Shelf " + (i + 1) + " ----------");
+                        s[i].output();
+                    }
+                    break;
+                }
+
+                case 3: {
+                    int flag = 0, pos = 0;
+                    System.out.print("Input name search: ");
+                    String name = sc.nextLine();
+                    for (int i = 0; i < amount; i++) {
+                        if (s[i].getName().equalsIgnoreCase(name)) {
+                            flag = 1;
+                            pos = i + 1;
+                            break;
+                        }
+                    }
+                    if (flag == 1) {
+                        System.out.println("Exist shelf in position " + pos);
+                    }
+                    else {
+                        System.err.println("Shelf Not Found");
+                    }
+                    break;
+                }
+
+                case 4: {
+                    System.out.print("Input name search: ");
+                    String name = sc.nextLine();
+                    for (int i = 0; i < amount; i++) {
+                        if(s[i].getName().equalsIgnoreCase(name))
+                            s[i].input();
+                    }
+                    break;
+                }
+
+                case 5: {
+                    break;
+                }
+
+                case 6: {
+                    break;
+                }
+
+                case 7: {
+                    break;
+                }
+
+                case 8: {
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+//        Shelf s1 = new Shelf();
+//        s1.input();
+//        s1.output();
+        Shelf s[] = new Shelf[500];
+        app(s);
+    }
+}
